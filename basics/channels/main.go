a9d04e558f93b26a8926890e4e49daf52c200729
salmanbarani <salman.barani@postpay.io>
2023-07-31 00:28:37 +0300

    play with channels

--- basics/channels/main.go
+++ basics/channels/main.go
@@ -13,17 +13,25 @@ func main() {
 		"http://golang.org",
 	}
 
+	c := make(chan string) // channel
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for { // infinit loop
+		go checkLink(<-c, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
